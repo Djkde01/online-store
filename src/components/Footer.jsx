6d1125b44b4a69a9cbd39547e10ff72d8741c884
Djkde01 <sergio.estrella@utp.edu.co>
2023-06-27 17:31:25 -0500

    feat (Footer component)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import "../styles/Footer.css";
+
+export function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Made with{" "}
+        <span role="img" aria-label="love">
+          ❤️
+        </span>{" "}
+        by <a href="https://twitter.com/djkde">Djkde</a>
+      </p>
+    </footer>
+  );
+}
