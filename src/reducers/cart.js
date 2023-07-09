33da165f6405aa00209b474ad76785e7c50ed88d
Djkde01 <sergio.estrella@utp.edu.co>
2023-07-08 20:13:35 -0500

    feat (Local storage update)

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,9 +1,14 @@
-export const cartIntialState = [];
+export const cartIntialState = JSON.parse(localStorage.getItem("cart")) || [];
 
 export const CART_ACTIONS = {
   ADD_TO_CART: "ADD_TO_CART",
   REMOVE_FROM_CART: "REMOVE_FROM_CART",
   CLEAR_CART: "CLEAR_CART",
+};
+
+export const updateLocalStorage = (cart) => {
+  console.log("updateLocalStorage", cart);
+  localStorage.setItem("cart", JSON.stringify(cart));
 };
 
 export const cartReducer = (state, action) => {
@@ -17,9 +22,12 @@ export const cartReducer = (state, action) => {
         // Use structured cloning to create a new array
         const newCart = structuredClone(state);
         newCart[productIndex].quantity += 1;
+        updateLocalStorage(newCart);
         return newCart;
       } else {
-        return [...state, { ...payload, quantity: 1 }];
+        const newCart = [...state, { ...payload, quantity: 1 }];
+        updateLocalStorage(newCart);
+        return newCart;
       }
     }
     case CART_ACTIONS.REMOVE_FROM_CART: {
@@ -32,12 +40,15 @@ export const cartReducer = (state, action) => {
         if (newCart[productIndex].quantity === 0) {
           newCart.splice(productIndex, 1);
         }
+        updateLocalStorage(newCart);
         return newCart;
       }
       break;
     }
-    case CART_ACTIONS.CLEAR_CART:
+    case CART_ACTIONS.CLEAR_CART: {
+      updateLocalStorage([]);
       return cartIntialState;
+    }
   }
   return state;
 };
